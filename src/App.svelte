4e3fce348587f6290aae8ef98b45a7e3009639ad
Muhammad Husnain <54207663+m-h9@users.noreply.github.com>
2022-01-03 00:34:36 +0500

    prac

---
+++ src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+  import { setContext } from "svelte";
+  import Todo from "./Todo.svelte";
+  let any = { name: "husnain" };
+  setContext("key", any);
+
+  let array = [];
+  let text = "";
+  function addContent() {
+    array = [...array, text];
+    text = "";
+  }
+</script>
+
+<div class="container">
+  <main>
+    <ul>
+      {#each array as a}
+        <Todo text={a} />
+      {/each}
+    </ul>
+    <div class="inputContainer">
+      <input type="text" bind:value={text} />
+      <button on:click={addContent}>Add</button>
+    </div>
+  </main>
+</div>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  main {
+    padding: 20px;
+    width: 450px;
+    min-height: 100px;
+  }
+  input {
+    width: 300px;
+    height: 40px;
+    padding-left: 5px;
+  }
+  button {
+    width: 120px;
+    height: 40px;
+    cursor: pointer;
+    font-size: larger;
+    font-weight: 600;
+  }
+</style>
---
+++ src/Todo.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { getContext } from "svelte";
+  let some = getContext("key");
+  console.log(some);
+  export let text;
+</script>
+
+<li>
+  {text}
+</li>
+
+<style>
+  li {
+    list-style: none;
+  }
+</style>
